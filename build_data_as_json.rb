f4be333409cac29ffc49e6ad9ce78b770b367480
Stefan Kendall <stefankendall@gmail.com>
2013-10-20 21:40:58 -0400

    Fixed data. Added x/y data.

--- build_data_as_json.rb
+++ build_data_as_json.rb
@@ -5,7 +5,11 @@ Find.find('./data') do |filename|
   if filename != "./data"
     name = filename[filename.rindex('/')+1..filename.length]
     types = File.read(filename).split(',')
-    json[name] = types
+    stripped_types = []
+    for type in types
+      stripped_types << type.strip
+    end
+    json[name] = stripped_types
   end
 end
 File.open('pokemon.json', 'w') do |f|
